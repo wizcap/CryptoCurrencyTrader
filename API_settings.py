6c0989d244257ff945d6afc5d86282bfc118d150
Tom Snell <tstomsnell@gmail.com>
2017-09-21 11:56:56 +0100

    Pytrends fix for new version

---
+++ API_settings.py
@@ -0,0 +1,8 @@
+poloniex_API_key = "FL8KUXM0-9XQXHC4I-GBISOHDH-THAHG7HM"
+poloniex_API_secret =\
+    "252b69a0308d8296c50d4e7b872c70c0ae9632424648c6ea9f58390250d9db6e9ecb4b8d6380a7815fde1dd9269ad8e9d6b53286864e32ae3a87d33c400c60cf"
+google_username = "tomsnellscripts"
+google_password = "9Qw9CBihn1rS"
+client_id = 'ehD6EVK-7_FfMA'
+client_secret = '92jtdJ0gEkc2NcHfIXtsVXdDdcc'
+user_agent = 'Python Scraping App'
