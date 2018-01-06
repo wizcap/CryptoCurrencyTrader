96cf200fed8820ba9098176373da1e8486e7434c
Tom Snell <tstomsnell@googlemail.com>
2018-01-06 20:44:50 +0000

    Update strategy_evaluation.py

--- strategy_evaluation.py
+++ strategy_evaluation.py
@@ -1,284 +1,281 @@
-import numpy as np
-from matplotlib import pyplot as plt
-from sklearn.preprocessing import minmax_scale
+from time import time
+from data_input_processing import Data, train_test_validation_indices, generate_training_variables
+from strategy_evaluation import post_process_training_results, output_strategy_results
+from machine_learning import random_forest_fitting, svm_fitting, adaboost_fitting, gradient_boosting_fitting,\
+    extra_trees_fitting, tensorflow_fitting, tensorflow_sequence_fitting
+from sklearn.preprocessing import StandardScaler
+
+SEC_IN_DAY = 86400
 
 
-def strategy_profit(strategy_score, fractional_price, strategy_dictionary, low_threshold, up_threshold):
+def meta_fitting(fitting_inputs, fitting_targets, strategy_dictionary):
+    error = []
+    model = []
+    train_indices, test_indices, validation_indices = train_test_validation_indices(
+        fitting_inputs,
+        strategy_dictionary['train_validation_test_ratios'])
 
-    """calculate net profit of trading strategy """
+    target_scaler = StandardScaler()
 
-    buy_sell_length = len(strategy_score)
-    portfolio_value = np.ones(buy_sell_length)
-    cash_value = np.zeros(buy_sell_length)
-    crypto_value = np.zeros(buy_sell_length)
-    effective_fee_factor = (strategy_dictionary['transaction_fee'] + 0.5 * strategy_dictionary['bid_ask_spread'])
+    if strategy_dictionary['regression_mode'] == 'regression':
+        fitting_targets = target_scaler.fit_transform(fitting_targets.reshape(-1, 1)).ravel()
 
-    n_trades = 0
-    curr_val = 1
-    stop_val = 1
+    if strategy_dictionary['ml_mode'] == 'svm':
+        model, error = svm_fitting(
+            fitting_inputs,
+            fitting_targets,
+            train_indices,
+            strategy_dictionary)
 
-    strategy_score = normalise_and_centre_score(strategy_score, up_threshold, low_threshold)
+    elif strategy_dictionary['ml_mode'] == 'randomforest':
+        model, error = random_forest_fitting(
+            fitting_inputs,
+            fitting_targets,
+            train_indices,
+            strategy_dictionary)
 
-    cash_value[0] = 1 - strategy_score[0]
-    crypto_value[0] = strategy_score[0] * (1 - effective_fee_factor)
+    elif strategy_dictionary['ml_mode'] == 'adaboost':
+        model, error = adaboost_fitting(
+            fitting_inputs,
+            fitting_targets,
+            train_indices,
+            strategy_dictionary)
 
-    for index in range(1, buy_sell_length):
-        crypto_value[index] = crypto_value[index - 1] * fractional_price[index - 1]
-        cash_value[index] = cash_value[index - 1]
-        portfolio_value[index] = crypto_value[index] + cash_value[index]
+    elif strategy_dictionary['ml_mode'] == 'gradientboosting':
+        model, error = gradient_boosting_fitting(
+            fitting_inputs,
+            fitting_targets,
+            train_indices,
+            strategy_dictionary)
 
-        curr_val *= fractional_price[index - 1]
+    elif strategy_dictionary['ml_mode'] == 'extratreesfitting':
+        model, error = extra_trees_fitting(
+            fitting_inputs,
+            fitting_targets,
+            train_indices,
+            strategy_dictionary)
 
-        score_step = (strategy_score[index] - strategy_score[index - 1])
+    if len(test_indices) != 0:
+        training_strategy_score = model.predict(fitting_inputs[train_indices, :])
+        test_strategy_score = model.predict(fitting_inputs[test_indices, :])
+        validation_strategy_score = model.predict(fitting_inputs[validation_indices, :])
 
-        score_step = portfolio_value[index] * score_step
+    else:
+        test_strategy_score = []
+        validation_strategy_score = []
 
-        #Stoploss
-        stop_val = max(stop_val, curr_val)
+    if strategy_dictionary['regression_mode'] == 'regression':
+        fitting_dictionary = {
+            'training_strategy_score': training_strategy_score,
+            'test_strategy_score': test_strategy_score,
+            'validation_strategy_score':validation_strategy_score,
+            'train_indices': train_indices,
+            'test_indices': test_indices,
+            'validation_indices': validation_indices,
+            'error': error}
 
-        if (stop_val - curr_val) / stop_val < strategy_dictionary['stop_loss']:
-            strategy_score[index] = 0
+    elif strategy_dictionary['regression_mode'] == 'classification':
+        fitting_dictionary = {
+            'training_strategy_score': training_strategy_score,
+            'test_strategy_score': test_strategy_score,
+            'validation_strategy_score': validation_strategy_score,
+            'train_indices': train_indices,
+            'test_indices': test_indices,
+            'validation_indices': validation_indices,
+            'error': error}
 
-        if score_step > 0:
+    return fitting_dictionary
 
-            if score_step > cash_value[index]:
-                score_step = cash_value[index]
 
-            effective_fee = effective_fee_factor * score_step
+def input_processing(data_to_predict_local, strategy_dictionary):
 
-            cash_value[index] = cash_value[index] - score_step
-            crypto_value[index] = crypto_value[index] + score_step - effective_fee
+    """retrieve and process continuous and classification targets"""
 
-            n_trades += 1
+    fitting_inputs, continuous_targets, classification_targets = generate_training_variables(
+        data_to_predict_local,
+        strategy_dictionary)
 
-        elif score_step < 0:
+    return fitting_inputs, continuous_targets, classification_targets
 
-            score_step = abs(score_step)
 
-            if score_step > crypto_value[index]:
-                score_step = crypto_value[index]
+def tic():
+    t = time()
+    return lambda: (time() - t)
 
-            effective_fee = effective_fee_factor * score_step
 
-            cash_value[index] = cash_value[index] + score_step - effective_fee
-            crypto_value[index] = crypto_value[index] - score_step
+def retrieve_data(ticker, scraper_currency, strategy_dictionary, filename):
+    data_local = None
+    while data_local is None:
+        try:
+            if strategy_dictionary['web_flag']:
+                end = time() - strategy_dictionary['offset'] * SEC_IN_DAY
 
-            n_trades += 1
+                start = end - SEC_IN_DAY * strategy_dictionary['n_days']
 
-    return portfolio_value, n_trades, cash_value, crypto_value, strategy_score
+                data_local = Data(
+                    ticker,
+                    scraper_currency,
+                    strategy_dictionary['candle_size'],
+                    strategy_dictionary['web_flag'],
+                    start=start,
+                    end=end,
+                )
+
+            else:
+                data_local = Data(
+                    ticker, scraper_currency,
+                    strategy_dictionary['candle_size'],
+                    strategy_dictionary['web_flag'],
+                    offset=strategy_dictionary['offset'],
+                    filename=filename,
+                    n_days=strategy_dictionary['n_days'])
+
+        except Exception:
+            pass
+
+    data_local.normalise_data()
+
+    return data_local
+
+
+def offset_scan_validation(strategy_dictionary, data_to_predict, fitting_inputs, fitting_targets, offsets):
+
+    """repeat fitting at a range of earlier offset start times to check for overfitting"""
+
+    strategy_dictionary['plot_flag'] = False
+    strategy_dictionary['ouput_flag'] = True
+
+    total_error = 0
+    total_profit = 0
+
+    for offset in offsets:
+        strategy_dictionary['offset'] = offset
+        fitting_dictionary,  profit_factor, test_profit_factor, _ = fit_strategy(
+            strategy_dictionary,
+            data_to_predict,
+            fitting_inputs,
+            fitting_targets)
+        total_error += fitting_dictionary['error'] / len(offsets)
+        total_profit += profit_factor
+
+    underlined_output('Averages: ')
+    print 'Total profit: ', total_profit
+    print 'Average error: ', total_error
+
+
+def tensorflow_offset_scan_validation(strategy_dictionary, data_to_predict, fitting_inputs, fitting_targets, offsets):
+
+    """repeat tensorflow fitting at a range of earlier offset start times to check for overfitting"""
+
+    strategy_dictionary['plot_flag'] = False
+    strategy_dictionary['ouput_flag'] = True
+    
+    total_error = 0
+    total_profit = 0
+
+    for offset in offsets:
+        strategy_dictionary['offset'] = offset
+        fitting_dictionary, error, profit_fraction = fit_tensorflow(
+            strategy_dictionary, data_to_predict, fitting_inputs, fitting_targets)
+        total_error += error
+        total_profit += profit_fraction
+
+    underlined_output('Averages: ')
+    print 'Total profit: ', total_profit
+    print 'Average error: ', total_error
+
+
+def import_data(strategy_dictionary):
+
+    data_to_predict = retrieve_data(
+        strategy_dictionary['ticker_1'],
+        strategy_dictionary['scraper_currency_1'],
+        strategy_dictionary,
+        strategy_dictionary['filename1'])
+
+    return data_to_predict
+
+
+def fit_strategy(strategy_dictionary, data_to_predict, fitting_inputs, fitting_targets):
+
+    """fit machine learning algorithm to data and return predictions and profit"""
+
+    toc = tic()
+
+    fitting_dictionary = meta_fitting(fitting_inputs, fitting_targets, strategy_dictionary)
+
+    fitting_dictionary, strategy_dictionary = post_process_training_results(
+        strategy_dictionary,
+        fitting_dictionary,
+        data_to_predict)
+
+    profit_factor, test_profit_factor = output_strategy_results(
+        strategy_dictionary,
+        fitting_dictionary,
+        data_to_predict,
+        toc)
+
+    return fitting_dictionary, profit_factor, test_profit_factor, strategy_dictionary
+
+
+def fit_tensorflow(strategy_dictionary, data_to_predict, fitting_inputs, fitting_targets):
+
+    """fit with tensorflow and return predictions and profit"""
+
+    toc = tic()
+
+    train_indices, test_indices, validation_indices = train_test_validation_indices(
+        fitting_inputs, strategy_dictionary['train_test_validation_ratios'])
+
+    if strategy_dictionary['sequence_flag']:
+        fitting_dictionary, error = tensorflow_sequence_fitting(
+            train_indices,
+            test_indices,
+            validation_indices,
+            fitting_inputs,
+            fitting_targets)
+
+    else:
+        fitting_dictionary, error = tensorflow_fitting(
+            train_indices,
+            test_indices,
+            validation_indices,
+            fitting_inputs,
+            fitting_targets)
+
+    fitting_dictionary['train_indices'] = train_indices
+    fitting_dictionary['test_indices'] = test_indices
+    fitting_dictionary['validation_indices'] = validation_indices
+
+    fitting_dictionary, strategy_dictionary = post_process_training_results(
+        strategy_dictionary,
+        fitting_dictionary,
+        data_to_predict)
+
+    profit_factor = output_strategy_results(strategy_dictionary, fitting_dictionary, data_to_predict, toc)
+    return fitting_dictionary, error, profit_factor
+
+
+def underlined_output(string):
+
+    """underline printed output"""
+
+    print string
+    print '----------------------'
+    print '\n'
 
 
 def normalise_and_centre_score(strategy_score, up_threshold, low_threshold):
 
     """normalise and centre score when fitting thresholds"""
 
-    temp_score = minmax_scale(strategy_score)
-
-    temp_score = temp_score - 0.5
+    temp_score = strategy_score
     temp_score[temp_score > up_threshold] = up_threshold
     temp_score[temp_score < -up_threshold] = -up_threshold
     temp_score[abs(temp_score) < low_threshold] = 0
+    temp_score = temp_score / (2 * up_threshold)
     temp_score = temp_score + 0.5
 
     return temp_score
 
 
-def fit_trade_threshold(strategy_score, fractional_price, strategy_dictionary):
-
-    """ fit minimum signal change to execute trade """
-
-    threshold_range = np.linspace(0, 1, 30)
-
-    best_profit = -1
-    best_up_threshold = 0
-    best_low_threshold = 0
-
-    for up_threshold in threshold_range:
-
-        low_threshold_range = threshold_range[threshold_range < up_threshold]
-
-        for low_threshold in low_threshold_range:
-            profit_vector, n_trades, _, _, _ = strategy_profit(
-                strategy_score,
-                fractional_price,
-                strategy_dictionary,
-                low_threshold,
-                up_threshold)
-            profit = strategy_profit_score(profit_vector, n_trades)
-
-            if profit > best_profit and n_trades != 0:
-                best_low_threshold = low_threshold
-                best_up_threshold = up_threshold
-                best_profit = profit
-
-        strategy_dictionary['low_threshold'] = best_low_threshold
-        strategy_dictionary['up_threshold'] = best_up_threshold
-
-    return strategy_dictionary
-
-
-def post_process_training_results(strategy_dictionary, fitting_dictionary, data):
-
-    """return fitting dictionary containing training parameters"""
-
-    strategy_dictionary = fit_trade_threshold(
-        fitting_dictionary['validation_strategy_score'],
-        data.fractional_close[fitting_dictionary['validation_indices']],
-        strategy_dictionary)
-
-    fitting_dictionary['portfolio_value'],\
-        fitting_dictionary['n_trades'],\
-        cash_value,\
-        crypto_value,\
-        fitting_dictionary['test_strategy_score']\
-        = strategy_profit(
-        fitting_dictionary['test_strategy_score'],
-        data.fractional_close[fitting_dictionary['test_indices']],
-        strategy_dictionary,
-        strategy_dictionary['low_threshold'],
-        strategy_dictionary['up_threshold'])
-
-    fitting_dictionary['validation_portfolio_value'],\
-        fitting_dictionary['validation_trades'],\
-        _,\
-        _,\
-        fitting_dictionary['validation_strategy_score']\
-        = strategy_profit(
-        fitting_dictionary['validation_strategy_score'],
-        data.fractional_close[fitting_dictionary['validation_indices']],
-        strategy_dictionary,
-        strategy_dictionary['low_threshold'],
-        strategy_dictionary['up_threshold'])
-
-    return fitting_dictionary, strategy_dictionary
-
-
-def strategy_profit_score(strategy_profit_local, number_of_trades):
-
-    """evaluate value added by the trading strategy overall"""
-
-    profit_fraction = strategy_profit_local[-1] / np.min(strategy_profit_local)
-    if number_of_trades == 0:
-        profit_fraction = -profit_fraction
-    return profit_fraction
-
-
-def draw_down(strategy_profit_local):
-
-    """find maximum drawdown of strategy"""
-
-    draw_down_temp = np.diff(strategy_profit_local)
-    draw_down_temp[draw_down_temp > 0] = 0
-    return np.mean(draw_down_temp)
-
-
-def profit_factor(portfolio_value, price):
-
-    """calculate profit of strategy compared to buy and hold """
-
-    return portfolio_value[-1] * price[0] / (portfolio_value[0] * price[-1]) - 1
-
-
-def output_strategy_results(strategy_dictionary, fitting_dictionary, data_to_predict, toc, momentum_dict=None):
-
-    """print or plot results of machine learning fitting"""
-
-    val_data = data_to_predict.close[fitting_dictionary['validation_indices']]
-    prediction_data = data_to_predict.close[fitting_dictionary['test_indices']]
-
-    profit = profit_factor(
-            fitting_dictionary['portfolio_value'],
-            prediction_data)
-
-    val_profit = profit_factor(
-            fitting_dictionary['validation_portfolio_value'],
-            val_data)
-
-    if strategy_dictionary['output_flag']:
-        print "Fitting time: ", toc()
-
-        print "Fractional profit compared to buy and hold: ", profit
-        print "Mean squared error: ", fitting_dictionary['error']
-        print "Window: ", strategy_dictionary['windows']
-        print "Target step :", strategy_dictionary['target_step']
-        print "Fitting model: ", strategy_dictionary['ml_mode']
-        print "Regression/classification: ", strategy_dictionary['regression_mode']
-        print "Number of trades: ", fitting_dictionary['n_trades']
-        print "Offset: ", strategy_dictionary['offset']
-
-        if momentum_dict is not None:
-            print "Simple Momentum Profit: ", profit_factor(
-            momentum_dict['portfolio_value'],
-            prediction_data)
-
-        print "\n"
-
-    if strategy_dictionary['plot_flag']:
-        plt.figure(1)
-        close_price, = plt.plot(prediction_data)
-        portfolio_value, = plt.plot(
-            prediction_data[0] * fitting_dictionary['portfolio_value'])
-
-        mom_strategy = []
-        if momentum_dict is not None:
-            mom_strategy, = plt.plot(prediction_data[0] * momentum_dict['portfolio_value'])
-
-        plt.legend([close_price, portfolio_value, mom_strategy], ['Close Price', 'Portfolio Value', 'Momentum Strategy'])
-        plt.xlabel('Candle number')
-        plt.ylabel('Exchange rate')
-
-        plt.figure(2)
-        validation_score, = plt.plot(np.squeeze(fitting_dictionary['validation_strategy_score']), )
-
-        momentum_score = []
-        if momentum_dict is not None:
-            momentum_score, = plt.plot(np.squeeze(momentum_dict['validation_score']), )
-
-        plt.legend([validation_score, momentum_score], ['Validation Score', 'Momentum Score'])
-
-        plt.show()
-    
-    return profit, val_profit
-
-
-def simple_momentum_comparison(data_obj, strategy_dictionary, fitting_dictionary):
-
-    """implement simple momentum strategy for comparison to machine learning"""
-
-    validation_indices = fitting_dictionary['validation_indices']
-    try:
-        validation_indices.remove(len(data_obj.fractional_close))
-
-    except:
-        pass
-
-    validation_indices\
-        = [v for v in validation_indices if v < min(len(data_obj.fractional_close), len(data_obj.mom_strategy))]
-    print len(data_obj.fractional_close)
-
-    test_data = data_obj.fractional_close[fitting_dictionary['test_indices']]
-    val_data = data_obj.fractional_close[validation_indices]
-
-    test_momentum = data_obj.mom_strategy[fitting_dictionary['test_indices']]
-    val_momentum = data_obj.mom_strategy[fitting_dictionary['validation_indices']]
-
-    momentum_dictionary = {}
-
-    strategy_dictionary = fit_trade_threshold(
-        test_momentum,
-        test_data,
-        strategy_dictionary)
-
-    momentum_dictionary['portfolio_value'],\
-        momentum_dictionary['n_trades'],\
-        _,\
-        _,\
-        momentum_dictionary['validation_score']\
-        = strategy_profit(
-        val_momentum,
-        val_data,
-        strategy_dictionary,
-        strategy_dictionary['low_threshold'],
-        strategy_dictionary['up_threshold'])
-
-    return momentum_dictionary
